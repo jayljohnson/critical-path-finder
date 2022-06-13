f605b642dfca350acd48e58d9be82c07d7d848d3
jayljohnson <jljohn00+github@gmail.com>
2022-06-12 18:19:24 -0700

    add milliseconds to logging, cleanup log txt

--- critical_path/critical_path.py
+++ critical_path/critical_path.py
@@ -6,9 +6,16 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from uuid import uuid4
 from csv import reader
+import optparse
 
 
-logging.basicConfig(filename='../target/example.log', encoding='utf-8', level=logging.INFO)
+logging.basicConfig(
+    filename='../target/logs/critical-path.log', 
+    encoding='utf-8', 
+    level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d %(levelname)s:\t%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
 class CriticalPath():
     """
@@ -26,8 +33,9 @@ class CriticalPath():
     def __init__(self, node_weights=None, graph=None):
         self.node_weights: typing.List[typing.List[any]] = node_weights
         self.graph: nx.DiGraph = graph
-        # This gets set after calling the run() method; it's also the CLI output
+        # These get set after calling the run() method; it's also the CLI output
         self.critical_path_edges = None
+        self.critical_path_length = None
 
 
     def load_graph(self, path):
@@ -35,7 +43,7 @@ class CriticalPath():
         Reads a dotviz .dot file representing a digraph.  
         Used by CLI -g flag to pass in a file path.
         """
-        logging.info("\nLoading graph from dot file")
+        logging.info("Loading graph from dot file")
         G = nx.DiGraph(nx.nx_pydot.read_dot(path))
         G.remove_node("\\n")
         logging.info(f"\tGraph loaded: {G}")
@@ -55,7 +63,7 @@ class CriticalPath():
             csv_reader = reader(read_obj)
             # Pass reader object to list() to get a list of lists
             node_weights = list(csv_reader)[1:]
-            logging.info(f"\nNode weight map from {path}: {node_weights}")
+            logging.info(f"Node weight map from {path}: {node_weights}")
             self.node_weights=node_weights
 
 
@@ -66,8 +74,9 @@ class CriticalPath():
         edge_weights = self._get_edge_weights()
         nx.set_edge_attributes(self.graph, edge_weights, self.EDGE_WEIGHT_ATTRIBUTE_NAME)
         longest_path_nodes = nx.dag_longest_path(self.graph)
+        self.critical_path_length = nx.dag_longest_path_length(self.graph)
         self.critical_path_edges = self._get_edges_from_ordered_list_of_nodes(longest_path_nodes)
-        return self.critical_path_edges
+        return {(u, v): edge_weights[(u, v)] for u, v in self.critical_path_edges}
 
 
     def save_image(self, path: str):
@@ -79,7 +88,7 @@ class CriticalPath():
         SAVE_PATH = path
         FILENAME_PREFIX = "CriticalPathGraph"
 
-        logging.info("\nDrawing graph")
+        logging.info("Drawing graph")
         # Set the default color for all nodes
         for u, v in self.graph.edges():
             self.graph[u][v]['color'] = EDGE_COLOR_DEFAULT
@@ -100,15 +109,15 @@ class CriticalPath():
         plt.clf()
 
 
-    def _get_edge_weights(self, default_weight: int = 1):
+    def _get_edge_weights(self, default_weight: int = 1) -> typing.Dict[tuple, int]:
         """
         Assign the same weight to all edges of u.  
         Needed for critical path calcs.
         """
         node_weight_map = dict([(u, int(w)) for u, w in self.node_weights])
-        logging.info(f"\nEdge weights for node: {node_weight_map}")
+        logging.info(f"Edge weights for node: {node_weight_map}")
         result = {(u, v): node_weight_map.get(u, default_weight) for u, v in self.graph.edges }
-        logging.info(f"\nEdge weights: {result}")
+        logging.info(f"Edge weights: {result}")
         return result
 
 
@@ -119,21 +128,21 @@ class CriticalPath():
         create a list of tuples in the same order 
         """
         edges = [(nodes[i], nodes[i+1]) for i in range(len(nodes)-1)]
-        logging.info(f"\nEdges from ordered list of nodes: {edges}") 
+        logging.info(f"Edges from ordered list of nodes: {edges}") 
 
         return edges
 
 
 if __name__ == "__main__":
 
-    def run():
+    def main():
         """
-        Calculate the critical path and save an image of the graph
-        Also does handling of cli arguments
+        Used by the CLI.
+        Calculate the critical path and save an image of the graph.
+        Requires the graph and weights to be stored as a file before running.
         """
-        logging.info("\n*** Calculating the critical path ***")
-        logging.info("\nParsing command line options.")
-        import optparse
+        logging.info("*** Calculating the critical path ***")
+        logging.info("Parsing command line options.")
         p = optparse.OptionParser()
         p.add_option('--graph', '-g', default="input/sample_graph.dot")
         p.add_option('--weights', '-w', default="input/sample_weights.csv")
@@ -154,4 +163,4 @@ if __name__ == "__main__":
         sys.stdout.write(str(critical_path))
         sys.exit(0)
     
-    run()
+    main()
